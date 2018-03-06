b4565d7336837776a6e6e230a5239ae273e83d5b
unknown <laxmanmanandhar7@gmail.com>
2018-03-06 21:14:38 +0545

    15_OPD query and  13_1 ANC visit count query added

---
+++ openmrs/reports/hmis/13-HIV_AIDS/13_1_Number_of_ANC_Visits.sql
@@ -0,0 +1,17 @@
+SELECT 
+    COUNT(DISTINCT (o1.person_id)) AS 'Number of ANC visits'
+FROM
+    obs o1
+        INNER JOIN
+    concept_name cn1 ON o1.concept_id = cn1.concept_id
+        AND cn1.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn1.name = 'ANC, ANC Visit'
+        AND o1.voided = 0
+        AND cn1.voided = 0
+        INNER JOIN
+    encounter e ON o1.encounter_id = e.encounter_id
+        INNER JOIN
+    person p1 ON o1.person_id = p1.person_id
+WHERE
+    DATE(e.encounter_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
+        AND o1.value_coded IS NOT NULL
