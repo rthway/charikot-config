6f86ae20ebe3775cea1a1d8bdfb5b3539a296f59
unknown <laxmanmanandhar7@gmail.com>
2018-03-09 14:47:46 +0545

    13_2 query edited

--- openmrs/reports/hmis/13-HIV_AIDS/13_2_PMTCT_of_HIV_ART_Started_HIV_Positive_Women_Male_Partner_Breast_Feeding_status.sql
+++ openmrs/reports/hmis/13-HIV_AIDS/13_2_PMTCT_of_HIV_ART_Started_HIV_Positive_Women_Male_Partner_Breast_Feeding_status.sql
@@ -1,25 +1,15 @@
 select
   question.concept_full_name as concept_name,
   answer.concept_full_name as answer_concept_name,
-  gender.gender as gender,
-  reporting_age_group.name as age_group,
-  reporting_age_group.sort_order as sort_order,
-  visit_type.type as visit_type,
   ifnull(result.total_count,0) as total_count
 from
   concept_view AS question
   INNER JOIN concept_answer ON question.concept_id = concept_answer.concept_id AND question.concept_full_name IN ('PMTCT, Started ART during','PMTCT, Who already know their HIV status','PMTCT, Breast feeding options opted by HIV +ve mother')
   INNER JOIN concept_view AS answer ON answer.concept_id = concept_answer.answer_concept
-  INNER JOIN (SELECT DISTINCT value_reference AS type FROM visit_attribute) visit_type 
-  INNER JOIN reporting_age_group ON reporting_age_group.report_group_name = 'All Ages'
-  INNER JOIN (SELECT 'M' as gender UNION SELECT 'F' AS gender UNION SELECT 'O' AS gender) as gender
   LEFT OUTER JOIN (
     SELECT
       obs.value_coded as answer_concept_id,
       obs.concept_id as question_concept_id,
-      person.gender as gender,
-      visit_attribute.value_reference as visit_type,
-      reporting_age_group.name as age_group,
       count(*) as total_count
     FROM
       obs
@@ -36,8 +26,4 @@ from
     group by obs.concept_id, obs.value_coded, reporting_age_group.name, person.gender, visit_attribute.value_reference
   ) result on question.concept_id = result.question_concept_id
               and answer.concept_id = result.answer_concept_id
-              and gender.gender = result.gender
-              and visit_type.type = result.visit_type
-              and result.age_group = reporting_age_group.name
-GROUP BY question.concept_full_name, answer.concept_full_name, gender.gender, reporting_age_group.name, visit_type.type
-ORDER BY reporting_age_group.sort_order;
+GROUP BY question.concept_full_name, answer.concept_full_name;
