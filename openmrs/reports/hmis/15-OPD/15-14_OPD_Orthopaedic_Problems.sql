d938be8e50c109e0a12b88025abfd26df3b78405
unknown <laxmanmanandhar7@gmail.com>
2018-03-11 16:23:59 +0545

    modified 15.4 query to adjut T14 values

--- openmrs/reports/hmis/15-OPD/15-14_OPD_Orthopaedic_Problems.sql
+++ openmrs/reports/hmis/15-OPD/15-14_OPD_Orthopaedic_Problems.sql
@@ -10,7 +10,7 @@ FROM
     FROM
         diagnosis_concept_view
     WHERE
-        icd10_code IN ('T14.2','T14.1','W19','V89','M06','M13','M19','M54.9')
+        icd10_code IN ('V89','M06','M13','M19','M54.9')
     ORDER BY FIELD(icd10_code, 'T14.2','T14.1','W19','V89','M06','M13','M19','M54.9')) c
         LEFT OUTER JOIN
     (SELECT 
@@ -39,9 +39,43 @@ FROM
         AND cn.voided = 0
     LEFT JOIN diagnosis_concept_view dcv ON
     dcv.concept_id = o.value_coded
-    and dcv.icd10_code IN ('T14.2','T14.1','W19','V89','M06','M13','M19','M54.9')
+    and dcv.icd10_code IN ('V89','M06','M13','M19','M54.9')
     WHERE
         p.voided = 0
     GROUP BY o.obs_id , dcv.icd10_code) a
     GROUP BY concept_full_name) b ON c.concept_full_name = b.concept_full_name
-;
+
+union all 
+SELECT 
+	 'Fracture/Fall/Injuries' as Disease,
+    'T14' as 'ICD CODE',
+    ifnull(SUM(male),0) AS Male, 
+    ifnull(SUM(Female),0) AS Female
+FROM
+    (SELECT 
+        concept_full_name,
+            COUNT(DISTINCT IF((gender = 'F'), obs_id, NULL)) AS Female,
+            COUNT(DISTINCT IF((gender = 'M'), obs_id, NULL)) AS Male
+    FROM
+        (SELECT 
+        dcv.concept_full_name, dcv.icd10_code, o.obs_id, p.gender
+    FROM
+        person p
+    INNER JOIN visit v ON p.person_id = v.patient_id
+        AND v.voided = 0
+    INNER JOIN encounter e ON v.visit_id = e.visit_id AND e.voided = 0
+    INNER JOIN obs o ON e.encounter_id = o.encounter_id
+        AND o.voided = 0
+        AND DATE(o.obs_datetime) BETWEEN '#startDate#' AND '#endDate'
+    INNER JOIN concept_name cn ON o.concept_id = cn.concept_id
+        AND cn.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn.name IN ('Non-coded Diagnosis' , 'Coded Diagnosis')
+        AND o.voided = 0
+        AND cn.voided = 0
+    LEFT JOIN diagnosis_concept_view dcv ON dcv.concept_id = o.value_coded
+        AND dcv.icd10_code IN ('T14.2' , 'T14.1', 'W19')
+    WHERE
+        p.voided = 0
+            AND dcv.icd10_code IS NOT NULL
+    GROUP BY o.obs_id , dcv.icd10_code) a
+    GROUP BY concept_full_name) b;
