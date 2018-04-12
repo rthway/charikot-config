65c237e4b92660dd39c1a534f199cdc6b64faf1f
suruchee <suruchee11@gmail.com>
2018-04-12 12:08:24 +0545

    changes made after some error solved for safe motherhood

--- openmrs/reports/hmis/06-Safe_Motherhood_Program/Safe_Abortion_Total.sql
+++ openmrs/reports/hmis/06-Safe_Motherhood_Program/Safe_Abortion_Total.sql
@@ -1,7 +1,7 @@
 SELECT 
- rag.name AS 'Age Group',
     first_answers.category AS 'Category',
-      COUNT(DISTINCT (first_concept.person_id)) AS 'Total Patietnt'
+    IFNULL(SUM(CASE WHEN age < 20  THEN 1 ELSE 0 END),0) AS 'less than 20',
+    IFNULL(SUM(CASE WHEN age >= 20 THEN 1 ELSE 0 END),0) AS 'greater than 20'
 
 FROM
     (SELECT 
@@ -17,7 +17,7 @@ FROM
     INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
     INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
         AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
-        AND answer_concept_fully_specified_name.name NOT IN ('NA')
+        AND answer_concept_fully_specified_name.name NOT IN ('Not Applicable')
         AND answer_concept_fully_specified_name.voided
         IS FALSE
     LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
@@ -28,14 +28,14 @@ FROM
         question_concept_name.name IN ('Safe abortion-Surgical procedure','Safe abortion-Medical Procedure')
             AND cd.name = 'Coded'
     ORDER BY answer_name DESC) first_answers
-        INNER JOIN
-    reporting_age_group rag ON rag.report_group_name = 'Safe Abortion'
         LEFT OUTER JOIN
     (SELECT 
         DISTINCT(o1.person_id),
             cn2.concept_id AS answer,
             cn1.concept_id AS question,
             v1.visit_id AS visit_id,
+               TIMESTAMPDIFF(YEAR, p1.birthdate, v1.date_started) AS age,
+
             v1.date_stopped AS datetime
     FROM
         obs o1
@@ -46,20 +46,14 @@ FROM
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn2.name NOT IN ('NA')
+        AND cn2.name NOT IN ('Not Applicable')
         AND cn2.voided = 0
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
+        INNER JOIN person p1 ON o1.person_id = p1.person_id
+
         AND v1.date_stopped IS NOT NULL
     WHERE
-        CAST(v1.date_stopped AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) first_concept ON first_concept.answer = first_answers.answer
+        CAST(v1.date_stopped AS DATE) BETWEEN DATE('2016-04-01') AND DATE('2018-04-01')) first_concept ON first_concept.answer = first_answers.answer
     
-        LEFT OUTER JOIN
-    person p ON first_concept.person_id = p.person_id
-        AND CAST(first_concept.datetime AS DATE) BETWEEN (DATE_ADD(DATE_ADD(p.birthdate,
-            INTERVAL rag.min_years YEAR),
-        INTERVAL rag.min_days DAY)) AND (DATE_ADD(DATE_ADD(p.birthdate,
-            INTERVAL rag.max_years YEAR),
-        INTERVAL rag.max_days DAY))
-        AND rag.report_group_name = 'Safe Abortion'
-GROUP BY  rag.name ,first_answers.category
+GROUP BY  first_answers.category
