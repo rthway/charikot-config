51ea50a128c698a7a30b27a752e917128f51c423
suruchee <suruchee11@gmail.com>
2018-04-19 15:18:10 +0545

    queries after Q/A safemotherhood

--- openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_2_TreatmentOutcome_PBC_Cases_Registered.sql
+++ openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_2_TreatmentOutcome_PBC_Cases_Registered.sql
@@ -1,12 +1,20 @@
-SELECT DISTINCT
- first_answers.category AS 'Category',
-         IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Condoms', 'Pills','Depo Provera') and second_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Short-Term',
-    IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Male sterilization','IUCD','Implant','Female sterilization') and second_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Long-Term'
-    FROM
+SELECT 
+    third_answers.answer_name AS 'Diagonasis',
+    gender.gender AS Gender,
+    SUM(CASE
+        WHEN
+            first_concept.answer IS NOT NULL
+                AND second_concept.answer IS NOT NULL
+                AND third_concept.answer IS NOT NULL
+                AND p.gender IS NOT NULL
+        THEN
+            1
+        ELSE 0
+    END) AS 'Patient Count'
+FROM
     (SELECT 
         ca.answer_concept AS answer,
-            IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name,
-            question_concept_name.name AS category
+            IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name
     FROM
         concept c
     INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
@@ -16,7 +24,6 @@ SELECT DISTINCT
     INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
     INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
         AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
-        AND answer_concept_fully_specified_name.name NOT IN ('Not Applicable')
         AND answer_concept_fully_specified_name.voided
         IS FALSE
     LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
@@ -24,7 +31,7 @@ SELECT DISTINCT
         AND answer_concept_short_name.voided
         IS FALSE
     WHERE
-        question_concept_name.name IN ('Safe abortion-Surgical procedure' , 'Safe abortion-Medical Procedure')
+        question_concept_name.name IN ('Tuberculosis, Treatment Outcome')
             AND cd.name = 'Coded'
     ORDER BY answer_name DESC) first_answers
         INNER JOIN
@@ -47,12 +54,14 @@ SELECT DISTINCT
         AND answer_concept_short_name.voided
         IS FALSE
     WHERE
-        question_concept_name.name IN ('Accepted Family Planning methods')
+        question_concept_name.name IN ('Tuberculosis, Diagnosis Category')
             AND cd.name = 'Coded'
-    ORDER BY answer_name DESC) second_answers
+    ORDER BY answer_name DESC) third_answers
+        INNER JOIN
+    (SELECT 'M' AS gender UNION SELECT 'F' AS gender) gender
         LEFT OUTER JOIN
     (SELECT 
-    DISTINCT (o1.person_id),
+       DISTINCT(o1.person_id),
             cn2.concept_id AS answer,
             cn1.concept_id AS question,
             v1.visit_id AS visit_id
@@ -60,27 +69,50 @@ SELECT DISTINCT
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name IN ('Safe abortion-Surgical procedure' , 'Safe abortion-Medical Procedure')
+        AND cn1.name = 'Tuberculosis, Treatment Outcome'
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn2.name NOT IN ('Not Applicable')
         AND cn2.voided = 0
-    INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
-    INNER JOIN visit v1 ON v1.visit_id = e.visit_id
-    WHERE
-        CAST(v1.date_started AS DATE)  BETWEEN DATE('#startDate#') AND DATE('#endDate#'))first_concept ON first_concept.answer = first_answers.answer
-         LEFT OUTER JOIN
+    INNER JOIN encounter e1 ON o1.encounter_id = e1.encounter_id
+    INNER JOIN visit v1 ON v1.visit_id = e1.visit_id
+    WHERE 
+        CAST(v1.date_started AS DATE) BETWEEN DATE(DATE_SUB('#startDate#', INTERVAL 12 MONTH)) AND DATE(DATE_SUB('#endDate#', INTERVAL 12 MONTH))) first_concept ON first_concept.answer = first_answers.answer
+        LEFT OUTER JOIN
     (SELECT 
-        DISTINCT(o1.person_id) ,
+        DISTINCT(o1.person_id),
             cn2.concept_id AS answer,
+            cn1.concept_id AS question,
             v1.visit_id AS visit_id
     FROM
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name = 'Accepted Family Planning methods'
+        AND cn1.name = 'Tuberculosis, Type'
+        AND o1.voided = 0
+        AND cn1.voided = 0
+    INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
+        AND cn2.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn2.name = 'Pulmonary BC'
+        AND cn2.voided = 0
+    INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
+    INNER JOIN visit v1 ON v1.visit_id = e.visit_id
+    WHERE
+        CAST(v1.date_started AS DATE) BETWEEN DATE(DATE_SUB('#startDate#', INTERVAL 12 MONTH)) AND DATE(DATE_SUB('#endDate#', INTERVAL 12 MONTH)))second_concept ON 
+        first_concept.person_id = second_concept.person_id
+        AND first_concept.visit_id = second_concept.visit_id
+        LEFT OUTER JOIN
+    (SELECT 
+        DISTINCT(o1.person_id),
+            cn2.concept_id AS answer,
+            cn1.concept_id AS question,
+            v1.visit_id AS visit_id
+            FROM
+        obs o1
+    INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
+        AND cn1.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn1.name = 'Tuberculosis, Diagnosis Category'
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
@@ -89,7 +121,13 @@ SELECT DISTINCT
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
     WHERE
-        CAST(v1.date_started AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON second_concept.answer = second_answers.answer
-        AND first_concept.person_id = second_concept.person_id
-        AND first_concept.visit_id = second_concept.visit_id
-	GROUP BY first_answers.category;
+      CAST(v1.date_started AS DATE) BETWEEN DATE(DATE_SUB('#startDate#', INTERVAL 12 MONTH)) AND DATE(DATE_SUB('#endDate#', INTERVAL 12 MONTH))) third_concept ON third_concept.answer = third_answers.answer
+        AND second_concept.person_id = third_concept.person_id
+        AND second_concept.visit_id = third_concept.visit_id
+        LEFT OUTER JOIN
+    person p ON first_concept.person_id = p.person_id
+        AND p.gender = gender.gender
+GROUP BY third_answers.answer_name,gender.gender
+	   
+	   
+	   
--- openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_2_TreatmentOutcome_PCD_EP_Others_Cases_Registration.sql
+++ openmrs/reports/hmis/11-Tuberculosis-Control-Program/11_2_TreatmentOutcome_PCD_EP_Others_Cases_Registration.sql
@@ -1,33 +1,17 @@
-SELECT DISTINCT
- first_answers.category AS 'Category',
-         IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Condoms', 'Pills','Depo Provera') and second_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Short-Term',
-    IFNULL(SUM(CASE WHEN second_answers.answer_name IN ('Male sterilization','IUCD','Implant','Female sterilization') and second_concept.person_id is not null THEN 1 ELSE 0 END),0) AS 'Long-Term'
-    FROM
-    (SELECT 
-        ca.answer_concept AS answer,
-            IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name,
-            question_concept_name.name AS category
-    FROM
-        concept c
-    INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
-    INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
-        AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
-        AND question_concept_name.voided IS FALSE
-    INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
-    INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
-        AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
-        AND answer_concept_fully_specified_name.name NOT IN ('Not Applicable')
-        AND answer_concept_fully_specified_name.voided
-        IS FALSE
-    LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
-        AND answer_concept_short_name.concept_name_type = 'SHORT'
-        AND answer_concept_short_name.voided
-        IS FALSE
-    WHERE
-        question_concept_name.name IN ('Safe abortion-Surgical procedure' , 'Safe abortion-Medical Procedure')
-            AND cd.name = 'Coded'
-    ORDER BY answer_name DESC) first_answers
-        INNER JOIN
+SELECT 
+    second_answers.answer_name AS 'Diagonasis',
+    gender.gender AS Gender,
+    SUM(CASE
+        WHEN
+            first_concept.answer IS NOT NULL
+                AND second_concept.answer IS NOT NULL
+                AND third_concept.answer IS NOT NULL
+                AND p.gender IS NOT NULL
+        THEN
+            1
+        ELSE 0
+    END) AS 'Patient Count'
+FROM
     (SELECT 
         ca.answer_concept AS answer,
             IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name
@@ -47,12 +31,39 @@ SELECT DISTINCT
         AND answer_concept_short_name.voided
         IS FALSE
     WHERE
-        question_concept_name.name IN ('Accepted Family Planning methods')
+        question_concept_name.name IN ('Tuberculosis, Treatment Outcome')
+            AND cd.name = 'Coded'
+    ORDER BY answer_name DESC) first_answers
+        INNER JOIN
+    (SELECT 
+        ca.answer_concept AS answer,
+            IFNULL(answer_concept_short_name.name, answer_concept_fully_specified_name.name) AS answer_name
+    FROM
+        concept c
+    INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
+    INNER JOIN concept_name question_concept_name ON c.concept_id = question_concept_name.concept_id
+        AND question_concept_name.concept_name_type = 'FULLY_SPECIFIED'
+        AND question_concept_name.voided IS FALSE
+    INNER JOIN concept_answer ca ON c.concept_id = ca.concept_id
+    INNER JOIN concept_name answer_concept_fully_specified_name ON ca.answer_concept = answer_concept_fully_specified_name.concept_id
+        AND answer_concept_fully_specified_name.concept_name_type = 'FULLY_SPECIFIED'
+        AND answer_concept_fully_specified_name.name IN ('Pulmonary CD','Extra Pulmonary')
+        AND answer_concept_fully_specified_name.voided
+        IS FALSE
+    LEFT JOIN concept_name answer_concept_short_name ON ca.answer_concept = answer_concept_short_name.concept_id
+        AND answer_concept_short_name.concept_name_type = 'SHORT'
+        AND answer_concept_short_name.voided
+        IS FALSE
+    WHERE
+        question_concept_name.name IN ('Tuberculosis, Type')
             AND cd.name = 'Coded'
     ORDER BY answer_name DESC) second_answers
+
+        INNER JOIN
+    (SELECT 'M' AS gender UNION SELECT 'F' AS gender) gender
         LEFT OUTER JOIN
     (SELECT 
-    DISTINCT (o1.person_id),
+        DISTINCT(o1.person_id),
             cn2.concept_id AS answer,
             cn1.concept_id AS question,
             v1.visit_id AS visit_id
@@ -60,36 +71,67 @@ SELECT DISTINCT
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name IN ('Safe abortion-Surgical procedure' , 'Safe abortion-Medical Procedure')
+        AND cn1.name = 'Tuberculosis, Treatment Outcome'
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn2.name NOT IN ('Not Applicable')
         AND cn2.voided = 0
-    INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
-    INNER JOIN visit v1 ON v1.visit_id = e.visit_id
+    INNER JOIN encounter e1 ON o1.encounter_id = e1.encounter_id
+    INNER JOIN visit v1 ON v1.visit_id = e1.visit_id
     WHERE
-        CAST(v1.date_started AS DATE)  BETWEEN DATE('#startDate#') AND DATE('#endDate#'))first_concept ON first_concept.answer = first_answers.answer
-         LEFT OUTER JOIN
+        CAST(v1.date_started AS DATE) BETWEEN DATE(DATE_SUB('#startDate#', INTERVAL 12 MONTH)) AND DATE(DATE_SUB('#endDate#', INTERVAL 12 MONTH))) first_concept ON first_concept.answer = first_answers.answer
+        LEFT OUTER JOIN
     (SELECT 
-        DISTINCT(o1.person_id) ,
+        DISTINCT(o1.person_id),
             cn2.concept_id AS answer,
+            cn1.concept_id AS question,
             v1.visit_id AS visit_id
     FROM
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name = 'Accepted Family Planning methods'
+        AND cn1.name = 'Tuberculosis, Type'
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn2.name IN ('Pulmonary CD','Extra Pulmonary')
+        AND cn2.voided = 0
+    INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
+    INNER JOIN visit v1 ON v1.visit_id = e.visit_id
+    WHERE       
+    CAST(v1.date_started AS DATE) BETWEEN DATE(DATE_SUB('#startDate#', INTERVAL 12 MONTH)) AND DATE(DATE_SUB('#endDate#', INTERVAL 12 MONTH)))second_concept ON second_concept.answer = second_answers.answer
+        AND first_concept.person_id = second_concept.person_id
+        AND first_concept.visit_id = second_concept.visit_id
+        LEFT OUTER JOIN
+    (SELECT 
+        DISTINCT(o1.person_id),
+            cn2.concept_id AS answer,
+            cn1.concept_id AS question,
+            v1.visit_id AS visit_id
+    FROM
+        obs o1
+    INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
+        AND cn1.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn1.name = 'Tuberculosis, Diagnosis Category'
+        AND o1.voided = 0
+        AND cn1.voided = 0
+    INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
+        AND cn2.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn2.name NOT IN ('New Diagnosis')
         AND cn2.voided = 0
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN visit v1 ON v1.visit_id = e.visit_id
     WHERE
-        CAST(v1.date_started AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON second_concept.answer = second_answers.answer
-        AND first_concept.person_id = second_concept.person_id
-        AND first_concept.visit_id = second_concept.visit_id
-	GROUP BY first_answers.category;
+      CAST(v1.date_started AS DATE) BETWEEN DATE(DATE_SUB('#startDate#', INTERVAL 12 MONTH)) AND DATE(DATE_SUB('#endDate#', INTERVAL 12 MONTH))) third_concept ON 
+         second_concept.person_id = third_concept.person_id
+        AND second_concept.visit_id = third_concept.visit_id
+        LEFT OUTER JOIN
+    person p ON first_concept.person_id = p.person_id
+        AND p.gender = gender.gender
+     
+GROUP BY second_answers.answer_name,gender.gender;
+	   
+	   
+	   
