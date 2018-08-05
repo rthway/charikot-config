ce3a649aa96b902dd3cd0a7405dc2f7f5cdb86fc
Dipak Thapa <dipakthapaofficial@gmail.com>
2018-08-05 18:00:18 +0545

    Replaces concrete date with generic

--- openmrs/reports/hmis/03-2_Nutrition_Acute_Malnutrition/Integrated_Management_of_Acute_Malnutrition_IMAM_Program.sql
+++ openmrs/reports/hmis/03-2_Nutrition_Acute_Malnutrition/Integrated_Management_of_Acute_Malnutrition_IMAM_Program.sql
@@ -1,18 +1,17 @@
-
-    SELECT
-        final.`Age Group` AS 'Age Group',
-        final.`Sex` AS 'Sex',
-        final.`Children at End of Last Month` AS 'Children at End of Last Month',
-        SUM(final.`New Admission`) AS 'New Admission',
-        SUM(final.`Re-admission`) AS 'Re-admission',
-        SUM(final.`Transfer In`) AS 'Transfer In',
-        SUM(final.`Discharge - Recovered`) AS 'Discharge - Recovered',
-        SUM(final.`Discharge - Death`) AS 'Discharge - Death',
-        SUM(final.`Discharge - Defaulter`) AS 'Discharge - Defaulter',
-        SUM(final.`Discharge - Not Improved`) AS 'Discharge - Not Improved',
-        SUM(final.`Discharge - Refer to Hospital`) AS 'Discharge - Refer to Hospital',
-        SUM(final.`Transfer Out`) AS 'Transfer Out',
-        (SUM(final.`Children at End of Last Month`) + SUM(final.`New Admission`) + SUM(final.`Re-admission`) + SUM(final.`Transfer In`) - SUM(final.`Discharge - Recovered`) - SUM(final.`Discharge - Death`) - SUM(final.`Discharge - Defaulter`) - SUM(final.`Discharge - Not Improved`) - SUM(final.`Discharge - Refer to Hospital`) - SUM(final.`Transfer Out`)) AS 'Children at End of This Month'
+SELECT
+    final.`Age Group` AS 'Age Group',
+    final.`Sex` AS 'Sex',
+    final.`Children at End of Last Month` AS 'Children at End of Last Month',
+    SUM(final.`New Admission`) AS 'New Admission',
+    SUM(final.`Re-admission`) AS 'Re-admission',
+    SUM(final.`Transfer In`) AS 'Transfer In',
+    SUM(final.`Discharge - Recovered`) AS 'Discharge - Recovered',
+    SUM(final.`Discharge - Death`) AS 'Discharge - Death',
+    SUM(final.`Discharge - Defaulter`) AS 'Discharge - Defaulter',
+    SUM(final.`Discharge - Not Improved`) AS 'Discharge - Not Improved',
+    SUM(final.`Discharge - Refer to Hospital`) AS 'Discharge - Refer to Hospital',
+    SUM(final.`Transfer Out`) AS 'Transfer Out',
+    (SUM(final.`Children at End of Last Month`) + SUM(final.`New Admission`) + SUM(final.`Re-admission`) + SUM(final.`Transfer In`) - SUM(final.`Discharge - Recovered`) - SUM(final.`Discharge - Death`) - SUM(final.`Discharge - Defaulter`) - SUM(final.`Discharge - Not Improved`) - SUM(final.`Discharge - Refer to Hospital`) - SUM(final.`Transfer Out`)) AS 'Children at End of This Month'
     FROM
         (SELECT 
             withoutDefaulters.*,
@@ -46,22 +45,21 @@
                 im.male_less_than_six AS maleLessThanSix,
                 im.female_more_than_six AS femaleMoreThanSix,
                 im.male_more_than_six AS maleMoreThanSix,
-                IF(oAdtType.obs_datetime >= DATE('2017-04-14'), p.person_id, NULL) thisMonthPatient,
-                IF(oAdtType.obs_datetime < DATE('2017-04-14'), p.person_id, NULL) lastMonthPatient
+                IF(oAdtType.obs_datetime >= DATE('#startDate#'), p.person_id, NULL) thisMonthPatient,
+                IF(oAdtType.obs_datetime < DATE('#startDate#'), p.person_id, NULL) lastMonthPatient
         FROM
             person p
         JOIN visit v ON p.person_id = v.patient_id
         JOIN encounter e ON v.visit_id = e.visit_id
         JOIN nonVoidedQuestionAnswerObs oAdtType ON e.encounter_id = oAdtType.encounter_id
-        LEFT JOIN imam im ON 1 = 1
+        INNER JOIN imam im ON 1 = 1
         WHERE
             ! p.voided AND ! v.voided AND ! e.voided
-                AND DATE(oAdtType.obs_datetime) BETWEEN DATE_SUB(DATE('2017-04-14'), INTERVAL 1 MONTH) AND DATE('2018-05-14')
+                AND DATE(oAdtType.obs_datetime) BETWEEN DATE_SUB(DATE('#startDate#'), INTERVAL 1 MONTH) AND DATE('#endDate#')
                 AND TIMESTAMPDIFF(MONTH, p.birthdate, v.date_started) < 60
                 AND oAdtType.question_full_name IN ('Admission Type' , 'Status At Discharge')
                 ) IMAM
         GROUP BY `Age Group` , `Sex`
-        -- ORDER BY  `Children at End of Last Month` DESC
         ) AS withoutDefaulters
         LEFT JOIN (SELECT 
             COUNT(DISTINCT t3.patient_id) AS `Count`,
@@ -72,7 +70,7 @@
         LEFT JOIN nonVoidedQuestionAnswerObs currentObs ON currentObs.person_id = prevObs.person_id
             AND currentObs.obs_datetime <= DATE_ADD(prevObs.obs_datetime, INTERVAL 28 DAY)
             AND currentObs.obs_datetime > prevObs.obs_datetime
-            AND DATE(currentObs.obs_datetime) <= '2018-05-14'
+            AND DATE(currentObs.obs_datetime) <= '#endDate#'
             AND currentObs.question_full_name = 'Admission Type'
         INNER JOIN patient_identifier t3 ON prevObs.person_id = t3.patient_id
             AND t3.identifier_type = 3
@@ -84,8 +82,8 @@
             AND ! p.voided
         WHERE
             currentObs.obs_id IS NULL
-                AND prevObs.obs_datetime >= DATE_SUB('2017-04-14', INTERVAL 28 DAY)
-                AND DATE(prevObs.obs_datetime) <= DATE_SUB('2018-05-14', INTERVAL 28 DAY)
+                AND prevObs.obs_datetime >= DATE_SUB('#startDate#', INTERVAL 28 DAY)
+                AND DATE(prevObs.obs_datetime) <= DATE_SUB('#endDate#', INTERVAL 28 DAY)
                 AND prevObs.question_full_name = 'Admission Type'
                 AND TIMESTAMPDIFF(MONTH, p.birthdate, v.date_started) < 60
         GROUP BY `Age Group` , `Sex`) AS defaultersCount ON defaultersCount.Sex = withoutDefaulters.Sex
