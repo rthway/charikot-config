f8f685fa00bc1db097fa70811603ff6052c287ca
suruchee <suruchee11@gmail.com>
2018-09-20 13:47:54 +0545

    update in ch for hospital indoor services

--- openmrs/reports/hmis/02_1_Immunization_Dataset/Immunized_children_count.sql
+++ openmrs/reports/hmis/02_1_Immunization_Dataset/Immunized_children_count.sql
@@ -1,7 +1,8 @@
 
 SELECT 
-    first_answers.answer_name AS 'Categories',
-    COUNT(DISTINCT (first_concept.person_id)) AS 'Total Patietnt'
+	first_answers.category AS 'Category',
+    first_answers.answer_name AS 'Type',
+    COUNT(DISTINCT (first_concept.person_id)) AS 'Total Patient'
 FROM
     (SELECT 
         ca.answer_concept AS answer,
@@ -23,11 +24,11 @@ FROM
         AND answer_concept_short_name.voided
         IS FALSE
     WHERE
-        question_concept_name.name IN ( 'Out Patient Details, Free Health Service Code')
+        question_concept_name.name IN ('ANC, TD Given','Vaccine-BCG Vaccine','Vaccine-DPT-HepB-Hib','Vaccine-OPV','Vaccine-IPV','Vaccine-Measles/rubella','Vaccine-Hepatitis B','Vaccine-JE','Vaccine-PCV')
             AND cd.name = 'Coded'
     ORDER BY answer_name DESC) first_answers
         LEFT OUTER JOIN
-    (SELECT DISTINCT
+    (SELECT 
         o1.person_id,
             cn2.concept_id AS answer,
             cn1.concept_id AS question
@@ -35,7 +36,7 @@ FROM
         obs o1
     INNER JOIN concept_name cn1 ON o1.concept_id = cn1.concept_id
         AND cn1.concept_name_type = 'FULLY_SPECIFIED'
-        AND cn1.name IN ('Out Patient Details, Free Health Service Code')
+        AND cn1.name IN ('ANC, TD Given','Vaccine-BCG Vaccine','Vaccine-DPT-HepB-Hib','Vaccine-OPV','Vaccine-IPV','	Vaccine-Measles/rubella','Vaccine-Hepatitis B','Vaccine-JE','Vaccine-PCV')
         AND o1.voided = 0
         AND cn1.voided = 0
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
@@ -45,7 +46,8 @@ FROM
     INNER JOIN person p1 ON o1.person_id = p1.person_id
       INNER JOIN visit v ON v.visit_id = e.visit_id
     WHERE
-   
-        DATE(e.encounter_datetime) BETWEEN DATE('#startDate#') AND DATE('#endDate#')
+    
+        DATE(v.date_started) BETWEEN '#startDate#' AND '#endDate#'
             AND o1.value_coded IS NOT NULL) first_concept ON first_concept.answer = first_answers.answer
-GROUP BY first_answers.answer_name
+GROUP BY first_answers.category,first_answers.answer_name
+ORDER BY first_answers.category, first_answers.answer_name;
